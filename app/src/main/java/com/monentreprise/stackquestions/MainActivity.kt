2d466a6889672e05e7870f74ad21cc2697887350
Jéwôm <j.clamentsanz@gmail.com>
2019-04-08 16:54:17 +0200

    first commit

--- app/src/main/java/com/monentreprise/stackquestions/MainActivity.kt
+++ app/src/main/java/com/monentreprise/stackquestions/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.monentreprise.stackquestions.api.RetrofitClient
 import com.monentreprise.stackquestions.api.models.Questions
+import org.jetbrains.anko.toast
 import retrofit2.Call
 import retrofit2.Response
 
@@ -16,9 +17,14 @@ class MainActivity : AppCompatActivity() {
         RetrofitClient().getClient().getQuestions().enqueue(object : retrofit2.Callback<Questions>{
 
             override fun onResponse(call: Call<Questions>, response: Response<Questions>) {
+                if (response.isSuccessful)
+                    toast(response.body()?.items?.get(0)?.title.toString())
+                else
+                    toast("not good response")
             }
 
             override fun onFailure(call: Call<Questions>, t: Throwable) {
+                toast("fail")
             }
 
         })
